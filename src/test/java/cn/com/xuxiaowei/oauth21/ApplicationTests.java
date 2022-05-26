74c0a601edaf21c8bed8921e19ca48d28bae51f2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-26 10:17:59 +0800

    init

---
+++ src/test/java/cn/com/xuxiaowei/oauth21/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.oauth21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
