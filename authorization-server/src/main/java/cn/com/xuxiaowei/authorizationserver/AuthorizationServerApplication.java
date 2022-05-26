3c32ad25d33fe674dc46af732e1b6e1f8837e669
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-26 15:14:58 +0800

    add SQL & see

--- authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/AuthorizationServerApplication.java
@@ -2,14 +2,41 @@ package cn.com.xuxiaowei.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.*;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationEndpointFilter;
 
 /**
  * 程序执行入口
+ * <p>
+ * 1. SQL<p>
+ * spring-security-oauth2-authorization-server-*.*.*.jar!\org\springframework\security\oauth2\server\authorization\client\oauth2-registered-client-schema.sql<p>
+ * spring-security-oauth2-authorization-server-*.*.*.jar!\org\springframework\security\oauth2\server\authorization\oauth2-authorization-consent-schema.sql<p>
+ * spring-security-oauth2-authorization-server-*.*.*.jar!\org\springframework\security\oauth2\server\authorization\oauth2-authorization-schema.sql<p>
+ * spring-security-oauth2-authorization-server-*.*.*.jar!\org\springframework\security\oauth2\server\authorization\client\oauth2-registered-client-schema.sql<p>
  *
  * @author xuxiaowei
- * @see <a href="http://127.0.0.1:1301/oauth2/authorize">/oauth2/authorize</a>
+ * @see OAuth2AuthorizationServerConfiguration OAuth 2.0 授权服务器支持的 {@link Configuration} 。
+ * @see UserDetailsServiceAutoConfiguration
  * @see OAuth2AuthorizationEndpointFilter /oauth2/authorize
+ * @see ProviderSettings#builder() /oauth2/authorize、/oauth2/token、/oauth2/jwks、/oauth2/revoke、/oauth2/introspect、/connect/register、/userinfo
+ * @see OAuth2AuthorizationService 此接口的实现负责OAuth 2.0 Authorization(s)的管理。
+ * @see InMemoryOAuth2AuthorizationService 一个 {@link OAuth2AuthorizationService} 存储 {@link OAuth2Authorization} 的内存。
+ * @see JdbcOAuth2AuthorizationService {@link OAuth2AuthorizationService} 的 JDBC 实现，它使用 {@link JdbcOperations} 进行 {@link OAuth2Authorization} 持久性。
+ * @see OAuth2AuthorizationConsentService 此接口的实现负责管理 {@link OAuth2AuthorizationConsent} OAuth 2.0 Authorization Consent(s) 。
+ * @see InMemoryOAuth2AuthorizationConsentService 一个 {@link OAuth2AuthorizationConsentService} 存储 {@link OAuth2AuthorizationConsent} 的内存。
+ * @see JdbcOAuth2AuthorizationConsentService {@link OAuth2AuthorizationConsentService} 的 JDBC 实现，它使用 {@link JdbcOperations} 进行 {@link OAuth2AuthorizationConsent} 持久性。
+ * @see RegisteredClientRepository OAuth 2.0 {@link RegisteredClient} (s) 的存储库。
+ * @see InMemoryRegisteredClientRepository 在内存中存储 {@link RegisteredClientRepository} (s) 的 {@link RegisteredClient} 。
+ * @see JdbcRegisteredClientRepository {@link RegisteredClientRepository} 的 JDBC 实现，它使用 {@link JdbcOperations} 进行 {@link RegisteredClient} 持久性。
  * @since 0.0.1
  */
 @SpringBootApplication
