e353c9dc30e8b2bc07777b1096facc96cfd99ba4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-26 19:23:57 +0800

    Configuration 授权码

--- authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/configuration/AuthorizationServerConfiguration.java
+++ authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/configuration/AuthorizationServerConfiguration.java
@@ -1,15 +1,36 @@
 package cn.com.xuxiaowei.authorizationserver.configuration;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 /**
  * 授权服务器配置
@@ -20,34 +41,100 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
  */
 @Slf4j
 @Configuration
-@Import({OAuth2AuthorizationServerConfiguration.class})
 public class AuthorizationServerConfiguration {
 
-    private JdbcOperations jdbcOperations;
-
-    @Autowired
-    public void setJdbcOperations(JdbcOperations jdbcOperations) {
-        this.jdbcOperations = jdbcOperations;
-    }
-
     /**
-     * 设置默认用户的密码
+     * @see <a href="https://docs.spring.io/spring-authorization-server/docs/current/reference/html/protocol-endpoints.html">协议端点的</a> Spring Security 过滤器链。
      */
-    @Autowired
-    public void setSecurityProperties(SecurityProperties securityProperties) {
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        String password = "123";
-        log.debug("默认用户名：user，密码：{}", password);
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
-        securityProperties.getUser().setPassword(password);
+        // 未从授权端点进行身份验证时重定向到登录页面
+        http.exceptionHandling((exceptions) -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+
+        return http.build();
     }
 
     /**
-     * 查询客户端 {@link Bean}
+     * @see <a href="https://docs.spring.io/spring-security/reference/servlet/authentication/index.html">用于身份验证</a> 的 Spring Security 过滤器链。
+     */
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // 表单登录处理从授权服务器过滤器链到登录页面的重定向
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()).formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    /**
+     * @see UserDetailsService 用于检索用户进行身份验证的实例。
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * @see RegisteredClientRepository 用于管理客户端的实例。
      */
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
-        return new JdbcRegisteredClientRepository(jdbcOperations);
+
+        RegisteredClient.Builder builder = RegisteredClient.withId(UUID.randomUUID().toString());
+        builder.clientId("xuxiaowei_client_id");
+        builder.clientSecret("{noop}xuxiaowei_client_secret");
+        builder.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+        builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
+        builder.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN);
+        builder.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS);
+        builder.redirectUri("http://127.0.0.1:1401/code");
+        builder.scope("snsapi_base");
+
+        RegisteredClient registeredClient = builder.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build()).build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    /**
+     * @see JWKSource 用于签署访问令牌的实例。
+     */
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    /**
+     * 启动时生成的带有密钥的实例 {@link KeyPair} 用于创建 {@link JWKSource} 上述内容。
+     */
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    /**
+     * {@link ProviderSettings} 配置 Spring Authorization Server 的实例。
+     */
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder().build();
     }
 
 }
