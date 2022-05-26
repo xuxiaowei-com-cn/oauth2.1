1e84b69ee9130f20384541cd2e5f26526d89183a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-26 15:16:41 +0800

    add 查询客户端 {@link Bean}

--- authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/configuration/AuthorizationServerConfiguration.java
+++ authorization-server/src/main/java/cn/com/xuxiaowei/authorizationserver/configuration/AuthorizationServerConfiguration.java
@@ -3,18 +3,36 @@ package cn.com.xuxiaowei.authorizationserver.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
 /**
  * 授权服务器配置
  *
  * @author xuxiaowei
+ * @see <a href="http://127.0.0.1:1301/oauth2/authorize?client_id=xuxiaowei_client_id&redirect_uri=http://127.0.0.1:1401/code&response_type=code&scope=snsapi_base&state=beff3dfc-bad8-40db-b25f-e5459e3d6ad7">/oauth2/authorize</a>
  * @since 0.0.1
  */
 @Slf4j
 @Configuration
+@Import({OAuth2AuthorizationServerConfiguration.class})
 public class AuthorizationServerConfiguration {
 
+    private JdbcOperations jdbcOperations;
+
+    @Autowired
+    public void setJdbcOperations(JdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
+    /**
+     * 设置默认用户的密码
+     */
     @Autowired
     public void setSecurityProperties(SecurityProperties securityProperties) {
 
@@ -22,6 +40,14 @@ public class AuthorizationServerConfiguration {
         log.debug("默认用户名：user，密码：{}", password);
 
         securityProperties.getUser().setPassword(password);
+    }
+
+    /**
+     * 查询客户端 {@link Bean}
+     */
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        return new JdbcRegisteredClientRepository(jdbcOperations);
     }
 
 }
