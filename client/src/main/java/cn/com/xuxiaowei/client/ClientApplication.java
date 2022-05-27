a9c4282b72ce4f1cecf2b437d2af7e1a6299447c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 08:18:37 +0800

    add client

---
+++ client/src/main/java/cn/com/xuxiaowei/client/ClientApplication.java
@@ -0,0 +1,19 @@
+package cn.com.xuxiaowei.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
---
+++ client/src/test/java/cn/com/xuxiaowei/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
