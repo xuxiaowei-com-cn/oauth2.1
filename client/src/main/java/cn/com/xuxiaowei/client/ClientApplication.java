53b58ead89f60875b6654b5fedce0e5da1531c49
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 16:13:57 +0800

    see

--- client/src/main/java/cn/com/xuxiaowei/client/ClientApplication.java
+++ client/src/main/java/cn/com/xuxiaowei/client/ClientApplication.java
@@ -2,14 +2,22 @@ package cn.com.xuxiaowei.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * 程序执行入口
  *
  * @author xuxiaowei
  * @see DefaultAuthorizationCodeTokenResponseClient#getTokenResponse(OAuth2AuthorizationCodeGrantRequest) 使用授权码获取Token
+ * @see org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationGrantRequestEntityUtils#getTokenRequestHeaders(ClientRegistration) 获取Token时的请求Headers
+ * @see HttpHeaders#encodeBasicAuth(String, String, Charset) 加密
+ * @see Base64.Decoder#decode(String, Charset) 解密（处理为 String：new String(Base64.getDecoder().decode(), StandardCharsets.ISO_8859_1)）
  * @since 0.0.1
  */
 @SpringBootApplication
