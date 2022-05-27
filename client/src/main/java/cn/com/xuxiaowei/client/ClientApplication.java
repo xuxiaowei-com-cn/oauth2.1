3814b98981fec56841ac2094fee516114cc10d2e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 15:43:31 +0800

    使用授权码获取Token

--- client/src/main/java/cn/com/xuxiaowei/client/ClientApplication.java
+++ client/src/main/java/cn/com/xuxiaowei/client/ClientApplication.java
@@ -2,11 +2,14 @@ package cn.com.xuxiaowei.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 
 /**
  * 程序执行入口
  *
  * @author xuxiaowei
+ * @see DefaultAuthorizationCodeTokenResponseClient#getTokenResponse(OAuth2AuthorizationCodeGrantRequest) 使用授权码获取Token
  * @since 0.0.1
  */
 @SpringBootApplication
