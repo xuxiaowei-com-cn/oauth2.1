a841b3e2e856636580e2ee8c0d10b7814023d83b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 09:27:50 +0800

    使用授权码 获取 Token

--- client/src/main/java/cn/com/xuxiaowei/client/controller/CodeRestController.java
+++ client/src/main/java/cn/com/xuxiaowei/client/controller/CodeRestController.java
@@ -1,7 +1,10 @@
 package cn.com.xuxiaowei.client.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +17,7 @@ import java.util.Map;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 @RestController
 public class CodeRestController {
 
@@ -29,6 +33,34 @@ public class CodeRestController {
     @RequestMapping("/code")
     public Map<String, Object> code(HttpServletRequest request, HttpServletResponse response, String code, String state) {
         Map<String, Object> map = new HashMap<>(4);
+
+        map.put("code", code);
+
+        log.info(code);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> param = new HashMap<>(8);
+        param.put("code", code);
+        param.put("client_id", "xuxiaowei_client_id");
+        param.put("client_secret", "xuxiaowei_client_secret");
+        param.put("redirect_uri", "http://127.0.0.1:1401/code");
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+
+        String accessTokenUri = "http://127.0.0.1:1301/oauth2/token" + "?code={code}&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&grant_type=authorization_code";
+
+        ResponseEntity<Map> responseEntity = restTemplate.postForEntity(accessTokenUri, httpEntity, Map.class, param);
+
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        log.info(String.valueOf(statusCode));
+
+        Map body = responseEntity.getBody();
+
+        log.info(String.valueOf(body));
 
         return map;
     }
